c685a749ff5a520e61a08955bee97c0cf25d097d
TobiasMBP <binnewiestobias@gmail.com>
2023-03-14 13:36:30 +0100

    Initial Commit

---
+++ RichTextEditor/RichTextEditor.h
@@ -0,0 +1,18 @@
+//
+//  RichTextEditor.h
+//  RichTextEditor
+//
+//  Created by Tobias Binnewies on 14.03.23.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for RichTextEditor.
+FOUNDATION_EXPORT double RichTextEditorVersionNumber;
+
+//! Project version string for RichTextEditor.
+FOUNDATION_EXPORT const unsigned char RichTextEditorVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <RichTextEditor/PublicHeader.h>
+
+
